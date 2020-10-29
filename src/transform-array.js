382af45668d45305ec2dbc879b61981a59bb2fc0
Alena Astravukh <helen151204@gmail.com>
2020-10-29 13:18:48 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!arr.length) return [];
+  if (arr.length === 0) return arr;
+  if (!Array.isArray(arr)) throw new Error;
+
+  let newArray = Array.from(arr);
+
+  let dn = "--discard-next";
+  let dp = "--discard-prev";
+  let dbn = "--double-next";
+  let dbp = "--double-prev";
+
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] === dn) {
+      if (i !== newArray.length - 1) {
+        newArray.splice(i, 2);
+        i = i - 2;
+      } else if (i === (newArray.length - 1)) {
+        newArray.splice(i, 1);
+        i--;
+      }
+    }
+
+    if (newArray[i] === dp) {
+      if (i !== 0) {
+        newArray.splice((i - 1), 2);
+        i = i - 2;
+      } else if (i === 0) {
+        newArray.splice(i, 1);
+        i--;
+      }
+    }
+
+    if (newArray[i] === dbn) {
+      if (i !== newArray.length - 1) {
+        newArray.splice(i, 1, newArray[i + 1]);
+      } else if (i === (newArray.length - 1)) {
+        newArray.splice(i, 1);
+      }
+    }
+
+    if (newArray[i] === dbp) {
+      if (i !== 0) {
+        newArray.splice(i, 1, newArray[i - 1]);
+      } else if (i === 0) {
+        newArray.splice(i, 1);
+      }
+    }
+  }
+    return newArray;
+
 };
